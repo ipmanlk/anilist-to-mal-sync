4fc49866dad3604a4b6e8242483f6ba4466d584b
ipmanlk <io@navinda.xyz>
2020-12-13 13:10:19 +0530

    change: export to current working directory

--- src/main/export.ts
+++ src/main/export.ts
@@ -5,21 +5,22 @@ import { getLists } from "../api/anilistApi";
 import { writeFileSync } from "fs";
 import { getMalXML } from "../util/format";
 
-const exportDir = `${__dirname}/../../exports`;
-
 export const exportLists = async () => {
 	let cache = Cache.getData();
-
 	if (!cache) {
 		await getLists().catch((e) => console.log(e));
 		cache = Cache.getData();
 	}
-
 	if (!cache) return;
-
 	const xml = getMalXML(cache);
-	writeFileSync(`${exportDir}/anime.xml`, xml.animeXML);
-	writeFileSync(`${exportDir}/manga.xml`, xml.mangaXML);
 
-	console.log(chalk.green("MAL Export has been completed!."));
+	const currentDir = process.cwd();
+
+	writeFileSync(`${currentDir}/anime.xml`, xml.animeXML);
+	writeFileSync(`${currentDir}/manga.xml`, xml.mangaXML);
+	console.log(
+		chalk.green(
+			`MAL Export has been completed!. Your files can be found in: ${currentDir}.`
+		)
+	);
 };
